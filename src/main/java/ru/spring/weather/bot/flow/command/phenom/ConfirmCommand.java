06c1cc24109a6592177b7df134c27894e00ad65a
posTour <dimidov496@gmail.com>
2024-08-10 20:53:38 +0300

    feat: проверка на добавление пользователем явления, которое уже отслеживается

--- src/main/java/ru/spring/weather/bot/flow/command/phenom/ConfirmCommand.java
+++ src/main/java/ru/spring/weather/bot/flow/command/phenom/ConfirmCommand.java
@@ -1,12 +1,14 @@
 package ru.spring.weather.bot.flow.command.phenom;
 
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.spring.weather.bot.dto.ViewPhenomDto;
 import ru.spring.weather.bot.flow.EntryBot;
 import ru.spring.weather.bot.flow.command.Command;
 import ru.spring.weather.bot.flow.stage.Stage;
 import ru.spring.weather.bot.storage.ChatState;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ConfirmCommand implements Command {
 
@@ -35,8 +37,14 @@ public class ConfirmCommand implements Command {
     public void acceptMessage(List<String> entries, ChatState chatState, EntryBot sender) throws TelegramApiException {
         switch (chatState.getCurrentStage()) {
             case CONFIRM_CREATION -> {
-                sendTextMessage(chatState, "Явление добавлено", sender);
-                // TODO: Отправка нового явления
+                // TODO: Получение явлений пользователя
+                var phenoms = chatState.getTrackedPhenoms();
+                if (checkIfExists(phenoms, chatState.getCity(), chatState.getPhenomType())) {
+                    sendTextMessage(chatState, "Вы уже добавляли такое явление", sender);
+                } else {
+                    sendTextMessage(chatState, "Явление добавлено", sender);
+                    // TODO: Отправка нового явления
+                }
             }
             case CONFIRM_REMOVAL -> {
                 sendTextMessage(chatState, "Явление удалено", sender);
@@ -46,5 +54,16 @@ public class ConfirmCommand implements Command {
         chatState.resetMenuMessageId();
         chatState.resetRequest();
         Command.enterStage(Stage.AUTH_MAIN_MENU, chatState, sender);
+    }
+
+    private boolean checkIfExists(List<ViewPhenomDto> phenoms, String city, String type) {
+        boolean ifExists = false;
+        for (var phenom : phenoms) {
+            if (Objects.equals(phenom.city(), city) && Objects.equals(phenom.type(), type)) {
+                ifExists = true;
+                break;
+            }
+        }
+        return ifExists;
     }
 }

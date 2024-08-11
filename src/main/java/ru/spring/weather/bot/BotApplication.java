57cfe5f0157e1117b6f1cf693bbe1201c76dc448
posTour <dimidov496@gmail.com>
2024-08-11 21:36:16 +0300

    feat: реализован клиент для общения с сервисом погоды

--- src/main/java/ru/spring/weather/bot/BotApplication.java
+++ src/main/java/ru/spring/weather/bot/BotApplication.java
@@ -2,8 +2,10 @@ package ru.spring.weather.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BotApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ru/spring/weather/bot/api/PhenomFeignClient.java
+++ src/main/java/ru/spring/weather/bot/api/PhenomFeignClient.java
@@ -1,8 +1,30 @@
 package ru.spring.weather.bot.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.spring.weather.bot.dto.CreationPhenomDto;
+import ru.spring.weather.bot.dto.UserDto;
+import ru.spring.weather.bot.dto.ViewPhenomDto;
+
+import java.util.List;
+import java.util.UUID;
 
 @FeignClient(value = "phenomFeignClient", url = "")
 public interface PhenomFeignClient {
+    @PostMapping(value = "")
+    ResponseEntity<Void> signUp(UserDto userDto);
+
+    @PostMapping(value = "")
+    ResponseEntity<Void> addPhenom(CreationPhenomDto creationPhenomDto);
+
+    @GetMapping(value = "")
+    List<ViewPhenomDto> getAllPhenomsByChatId(@PathVariable("chat_id") long chatId);
+
+    @DeleteMapping(value = "")
+    ResponseEntity<Void> deletePhenom(@PathVariable UUID id);
 }
 
